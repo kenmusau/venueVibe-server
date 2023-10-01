4d5efa76b152ad69f6b6bb84d4b93ebed954a9aa
Kennedy Musau <kennedy.musauu@gmail.com>
2023-10-01 23:56:49 +0300

    update space controller

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,2 +1,43 @@
 class SpacesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def index
+    render json: Space.all
+  end
+
+  def show
+    space = find_space
+    render json: space
+  end
+
+  def create
+    space = Space.create!(space_params)
+    render json: space, status: :created
+  end
+
+  def update
+    space = find_space
+    space.update!(space_params)
+    render json: space
+  end
+
+  def destroy
+    space = find_space
+    space.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_space
+    Space.find(params[:id])
+  end
+
+  def space_params
+    params.permit(:name, :location, :price, :status, :image, :description, :contact, :admin_id, :client_id)
+  end
+
+  def render_not_found_response
+    render json: { error: "Space not found" }, status: :not_found
+  end
 end
--- app/serializers/space_serializer.rb
+++ app/serializers/space_serializer.rb
@@ -1,5 +1,5 @@
 class SpaceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :price, :status, :image, :description, :contact
-  has_one :admin
-  has_one :client
+  attributes :id, :name, :location, :price, :status, :image, :description, :contact, :admin_id, :client_id
+  # has_one :admin
+  # has_one :client
 end
