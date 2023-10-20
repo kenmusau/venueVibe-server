2455514e3b4ae7ae836ac37fdec95108b3677560
Kennedy Musau <kennedy.musauu@gmail.com>
2023-10-21 01:15:15 +0300

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -21,7 +21,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    # credentials: true
   end
 end
