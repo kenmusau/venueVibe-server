0058ae7d2e71d2d4f4f8ed55c583036a0558ff5a
Kennedy Musau <kennedy.musauu@gmail.com>
2023-10-21 02:39:49 +0300

    update space controller

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -10,39 +10,40 @@ class SpacesController < ApplicationController
     render json: space
   end
 
-  # def create
-  #   space = Space.create!(space_params)
-  #   render json: space, status: :created
-  # end
   def create
-    space = Space.new(space_params)
-    if space.save
-      # Upload image to Cloudinary
-      space.image = Cloudinary::Uploader.upload(params[:image])
-      space.save
-      render json: space, status: :created
-    else
-      render json: space.errors, status: :unprocessable_entity
-    end
+    space = Space.create!(space_params)
+    render json: space, status: :created
   end
 
-  # def update
-  #   space = find_space
-  #   space.update!(space_params)
-  #   render json: space
+  # def create
+  #   space = Space.new(space_params)
+  #   if space.save
+  #     # Upload image to Cloudinary
+  #     space.image = Cloudinary::Uploader.upload(params[:image])
+  #     space.save
+  #     render json: space, status: :created
+  #   else
+  #     render json: space.errors, status: :unprocessable_entity
+  #   end
   # end
 
   def update
     space = find_space
-    if space.update(space_params)
-      # Upload image to Cloudinary
-      space.image = Cloudinary::Uploader.upload(params[:image])
-      space.save
-      render json: space
-    else
-      render json: space.errors, status: :unprocessable_entity
-    end
+    space.update!(space_params)
+    render json: space
   end
+
+  # def update
+  #   space = find_space
+  #   if space.update(space_params)
+  #     # Upload image to Cloudinary
+  #     space.image = Cloudinary::Uploader.upload(params[:image])
+  #     space.save
+  #     render json: space
+  #   else
+  #     render json: space.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   def destroy
     space = find_space
