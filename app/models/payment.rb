23d41ee81f811b92161eb88f03ea52dfff0c7583
Kennedy Musau <kennedy.musauu@gmail.com>
2023-10-20 10:41:21 +0300

    update custom validation

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,5 +3,5 @@ class Payment < ApplicationRecord
 
   validates :phone_number, presence: true
   validates :phone_number, format: { with: /\A254\d{9}\z/, message: "should start with 254 and consist of 12 digits." }
-  validates :phone_number, numericality: { only_integer: true, message: "should contain only numbers." }
+  validates :phone_number, numericality: { only_integer: true, message: "should only contain numbers." }
 end
